92e605d7bcbd1d7b6d9530d820fd3dc2796272ff
JULIAN RICARDO GONZALEZ QUIROGA <ricardo.gonzalez@est.iudigital.edu.co>
2025-09-14 12:35:33 -0500

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -183,6 +183,8 @@ END$$
 DELIMITER ;
 
 --UDF
+DELIMITER $$
+
 DROP FUNCTION IF EXISTS fn_promedio_presupuesto_por_docente;
 CREATE FUNCTION fn_promedio_presupuesto_por_docente(p_docente_id INT)
 RETURNS DECIMAL(12,2)
